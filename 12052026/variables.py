b283fc7b16ef09a013cee89a8bb2484eff9a16e1
pandoraxdn <pandoraxdn@protonmail.com>
2026-05-12 15:13:07 -0600

    Clase 12-05-2026

---
+++ 12052026/variables.py
@@ -0,0 +1,34 @@
+# Camellcase
+miVariable = ""
+nombreEmpleado = "Juanito Perez"
+calleEmpleado = "5 de mayo"
+
+# Dundercase
+mi_variable = "" # String/cadena
+nombre_empleado = "Juanita Lopez"
+calle_empleado = "5 de febrero"
+
+mi_variable = 15 # Lenguaje dinamico
+
+def hello():
+    print("Saludos IRD31")
+
+hello()
+
+# Scope / el alcance de las variables
+# Variables locales y variables globales
+nombre = "Azami Lazcano Lozano"
+
+def imprimir():
+    print("Variable global: ",nombre)
+    nombre_local = "Serena Lazcano Lozano"
+    print("Variable local: ",nombre_local)
+
+imprimir()
+
+
+
+
+
+
+
