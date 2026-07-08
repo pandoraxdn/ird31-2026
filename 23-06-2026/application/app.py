ffe4030dfa66226f107e2e851ef7e220ffdad3ac
Javier Eduardo <al222010046@gmail.com>
2026-07-08 09:16:05 -0600

    08-07-2026

--- 23-06-2026/application/app.py
+++ 23-06-2026/application/app.py
@@ -2,6 +2,10 @@ import sys
 from PySide6.QtWidgets import QApplication as App
 #from windows.LoginWindow import LoginWindow
 from windows.FormWindow import FormWindow
+from database.data import engine, Base
+from database.models import Usuario, Pedido, Producto, DetallePedido
+
+Base.metadata.create_all(bind=engine)
 
 def main():
     app = App()
---
+++ 23-06-2026/application/database/data.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "postgresql://postgres:123456@localhost:5432/ird31"
+#DATABASE_URL = 'mysql+pymysql://root:@localhost:3306/ird32'
+
+engine = create_engine(DATABASE_URL, echo=True)
+Base = declarative_base()
+SesionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
